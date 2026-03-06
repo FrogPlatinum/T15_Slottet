bd19f6b9205e7090f492faff3b592b82d5061631
Gavin Denver <gavindenver@gmail.com>
2026-03-06 10:31:09 +0100

    Created clean architecture pathing. Started ResidentSchema entity.

---
+++ SlottetDomain/Entity/ResidentSchema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlottetDomain.Enums;
+
+namespace SlottetDomain.Entity
+{
+    public class ResidentSchema
+    {
+        public string? Name { get; set; }
+        public TrafficLightStatus TrafficLight { get; set; }
+
+    }
+}
