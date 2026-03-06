3656ab9377553fb1c400d40390d3410df0dbb324
Amanda <amandajuhlsilberg@hotmail.com>
2026-03-06 12:24:32 +0100

    Changed name

--- SlottetTests/ResidentTests.cs
+++ SlottetTests/ResidentTests.cs
@@ -27,12 +27,12 @@ namespace SlottetTests
             Assert.AreEqual(1, ??.Schemas.Count);
 
             var addedPerson = ??.Schemas[0];
-            Assert.AreEqual(1, addedSchema.Id);
-            Assert.AreEqual("name", addedSchema.Name);
-            Assert.AreEqual(SlottetDomain.Enums.TrafficLightStatus.Red, addedSchema.TrafficLight);
-            Assert.AreEqual(1, addedSchema.MedicineStatuses);
-            Assert.AreEqual("Hans", addedSchema.Employee);
-            Assert.AreEqual("Glad i dag", addedSchema.Note);
+            Assert.AreEqual(1, newSchema.Id);
+            Assert.AreEqual("name", newSchema.Name);
+            Assert.AreEqual(SlottetDomain.Enums.TrafficLightStatus.Red, newSchema.TrafficLight);
+            Assert.AreEqual(1, newSchema.MedicineStatuses);
+            Assert.AreEqual("Hans", newSchema.Employee);
+            Assert.AreEqual("Glad i dag", newSchema.Note);
 
         }
     }
