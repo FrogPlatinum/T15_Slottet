5826d5a658147f1bb3badbb82d74ec73a10015ca
Gavin Denver <gavindenver@gmail.com>
2026-03-06 12:07:59 +0100

    - Fixed Resident Schema (ID) - Added ResidentSchema Interface - Added ResidentSchemaMemoryRepo

--- SlottetDomain/Entity/ResidentSchema.cs
+++ SlottetDomain/Entity/ResidentSchema.cs
@@ -9,8 +9,8 @@ namespace SlottetDomain.Entity
 {
     public class ResidentSchema
     {
-       
 
+        public int Id { get; set; }
         public string? Name { get; set; }
         public TrafficLightStatus TrafficLight { get; set; }
 
