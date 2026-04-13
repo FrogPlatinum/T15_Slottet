e171d3c3bdbe582aba0593d0cafbf6da3e6e4029
Amanda <amandajuhlsilberg@hotmail.com>
2026-04-13 13:13:10 +0200

    Tried some stuff.. But I'm pretty lost

---
+++ ResidentSchemaDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Slottet.Shared;
+public class ResidentSchemaDto
+{
+	public ResidentSchemaDto()
+	{
+
+    public string? Name { get; set; }
+    public enum TrafficLightStatus
+    {
+        Green,
+        Yellow,
+        Red
+    }
+    public TrafficLightStatus TrafficLight { get; set; }
+
+    public List<MedicineStatus> MedicineStatuses { get; set; }
+
+    public string Employee { get; set; }
+
+    public string Note { get; set; }
+}
+
