e0e998dca3ed3b042265a1a06d050b773145d155
Amanda <amandajuhlsilberg@hotmail.com>
2026-05-19 14:26:19 +0200

    Made a create form that works. Started making some new buttons, but commented them out.

--- Slottet.Blazor/Program.cs
+++ Slottet.Blazor/Program.cs
@@ -1,10 +1,13 @@
 using Slottet.Blazor.Components;
+using Slottet.Blazor.Components.Pages;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddHttpClient();
 
 
@@ -29,6 +32,7 @@ app.UseHttpsRedirection();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
