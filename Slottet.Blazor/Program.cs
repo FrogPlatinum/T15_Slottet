e3972448e581e4f01ce99974a80036c757c90d8b
Morten Lauritsen <m@morten-lauritsen.dk>
2026-05-20 11:08:19 +0200

    Added traffic light active status to set active color for resident

--- Slottet.Blazor/Program.cs
+++ Slottet.Blazor/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents();
 builder.Services.AddHttpClient();
-
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 builder.Services.AddScoped(sp => new HttpClient
 {
@@ -29,6 +30,7 @@ app.UseHttpsRedirection();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
